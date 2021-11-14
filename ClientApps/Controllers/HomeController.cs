884cc7a12902e7e9521d91cda58785888c980f53
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2021-11-14 15:28:36 +0600

    working

---
+++ ClientApps/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using ClientApps.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+
+namespace ClientApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var generateToken = JsonDataHelper.GenerateTokenJsonDataPost(jsonData: JsonConvert.SerializeObject(new { userName = "test", password = "test" }), url: "https://localhost:44382/gateway/users/authenticate");
+            //var jsonResponse = JsonDataHelper.JsonDataPost(jsonData: jsonData, fToken: tokenInfo.f_token, aToken: tokenInfo.access_token, url: ababilWebServiceUrl + "/ababil-customer/api/customers/individuals");
+
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Clear();
+            client.BaseAddress = new Uri("http://localhost:9000");
+            var jwt = "";
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {jwt}");
+
+
+            var usersResponse = client.GetAsync("/customers").Result;
+            var users = usersResponse.Content.ReadAsStringAsync().Result;
+
+            var res = client.GetAsync("/customers/1").Result;
+            var responseResultFor = res.Content.ReadAsStringAsync().Result;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ClientApps/JsonDataHelper.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using System.Net;
+
+namespace ClientApps
+{
+    public static class JsonDataHelper
+    {
+        public static string JsonDataPost(string jsonData, string fToken, string aToken, string url)
+        {
+            var result = string.Empty;
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "POST";
+            httpRequest.ContentType = "application/json";
+            httpRequest.Accept = "application/json";
+            httpRequest.Headers.Add("Authorization", $"Bearer {aToken}");
+            httpRequest.Headers.Add("F-TOKEN", $"{fToken}");
+
+            using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+            {
+                streamWriter.Write(jsonData);
+            }
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                result = streamReader.ReadToEnd();
+            }
+            if (httpResponse.StatusCode == HttpStatusCode.OK)
+                return result;
+            return result;
+        }
+
+        public static string GenerateTokenJsonDataPost(string jsonData, string url)
+        {
+            string responseResult = string.Empty;
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "POST";
+            httpRequest.ContentType = "application/json";
+            httpRequest.Accept = "application/json";
+            using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+            {
+                streamWriter.Write(jsonData);
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                responseResult = streamReader.ReadToEnd();
+            }
+            if (httpResponse.StatusCode == HttpStatusCode.OK)
+            {
+                //var ftoken = httpResponse.Headers["F-TOKEN"];
+                //return new KeyValuePair<string, string>(ftoken, responseResult);
+                return responseResult;
+            }
+            return responseResult;
+        }
+    }
+}
