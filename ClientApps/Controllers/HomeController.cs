5fd908c869174314dc79bb43449d9fbd6da356e2
eshohag <eshohag@outlook.com>
2021-12-06 22:42:51 +0600

    products

--- ClientApps/Controllers/HomeController.cs
+++ ClientApps/Controllers/HomeController.cs
@@ -21,8 +21,9 @@ namespace ClientApps.Controllers
         public IActionResult Index()
         {
             var authenticateUserInfo = JsonConvert.DeserializeObject<AuthenticateResponse>(JsonDataHelper.GetJsonResponseData(model: new { userName = "test", password = "test" }, url: "https://localhost:44382/gateway/users/authenticate", WebRequestMethods.Http.Post));
-            var jsonResponse = JsonDataHelper.GetJsonResponseData(model:null, url: "https://localhost:44382/gateway/users/getall", WebRequestMethods.Http.Get, token: authenticateUserInfo.JwtToken);
+            var users = JsonDataHelper.GetJsonResponseData(model:null, url: "https://localhost:44382/gateway/users/getall", WebRequestMethods.Http.Get, token: authenticateUserInfo.JwtToken);
             var customers = JsonDataHelper.GetJsonResponseData(model: null, url: "https://localhost:44382/gateway/customer", WebRequestMethods.Http.Get, token: authenticateUserInfo.JwtToken);
+            var products = JsonDataHelper.GetJsonResponseData(model: null, url: "https://localhost:44382/gateway/product", WebRequestMethods.Http.Get, token: authenticateUserInfo.JwtToken);
 
             return View();
         }
