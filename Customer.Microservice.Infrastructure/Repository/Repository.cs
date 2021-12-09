5b84a16b0f87ac71cd9bce31c5bc01bae42efb16
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2021-12-09 10:58:20 +0600

    repository

---
+++ Customer.Microservice.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,515 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Customer.Microservice.Infrastructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        #region Members
+
+        private DbContext _context;
+        private bool _disposed;
+
+        #endregion
+
+        #region Constructor
+        public Repository(DbContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            _context = context;
+            _context.Database.SetCommandTimeout(100000);
+        }
+        #endregion
+
+        #region PROPERTY
+        protected DbSet<T> DbSet
+        {
+            get { return _context.Set<T>(); }
+        }
+
+        #endregion
+
+        #region LINQ QUERY
+        public virtual void Add(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+
+            DbSet.Add(item); // add new item in this set
+        }
+
+        public virtual void Remove(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+
+            Attach(item); 
+            DbSet.Remove(item);
+
+        }
+        public virtual void Modify(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+            var entry = _context.Entry(item);
+            DbSet.Attach(item);
+            entry.State = EntityState.Modified;
+        }
+
+        public T SingleOrDefault(Expression<Func<T, bool>> predicate)
+        {
+            return DbSet.SingleOrDefault(predicate);
+        }
+
+        public virtual IQueryable<T> All()
+        {
+            return DbSet.AsQueryable().AsNoTracking();
+        }
+
+        public T Create(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+            DbSet.Add(item);
+            SaveChanges();
+            return item;
+        }
+
+        public int Update(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+            var entry = _context.Entry(item);
+            DbSet.Attach(item);
+            entry.State = EntityState.Modified;
+            return SaveChanges();
+        }
+        public int Update(Expression<Func<T, bool>> predicate)
+        {
+            var records = Where(predicate);
+            if (!records.Any())
+            {
+                throw new Exception();
+            }
+            foreach (var record in records)
+            {
+                var entry = _context.Entry(record);
+
+                DbSet.Attach(record);
+
+                entry.State = EntityState.Modified;
+            }
+            return SaveChanges();
+        }
+
+        /*   
+          Calling Example- 
+         _repository.Update(obj, o => o);
+         _repository.Update(obj, o => o.Name, o => o.Description);
+
+         */
+        public int Update(T entity, params Expression<Func<T, object>>[] updatedProperties)
+        {
+            //Ensure only modified fields are updated
+            var dbEntityEntry = _context.Entry(entity);
+            DbSet.Attach(entity);
+
+            if (updatedProperties.Any())
+            {
+                //update explicitly mentioned properties
+                foreach (var property in updatedProperties)
+                {
+                    dbEntityEntry.Property(property).IsModified = true;
+                }
+            }
+            else
+            {
+                //No items mentioned, so find out the updated entries
+                //foreach (var property in dbEntityEntry.OriginalValues)
+                //{
+                //    var original = dbEntityEntry.OriginalValues.GetValue<object>(property);
+                //    var current = dbEntityEntry.CurrentValues.GetValue<object>(property);
+                //    if (original != null && !original.Equals(current))
+                //        dbEntityEntry.Property(property).IsModified = true;
+                //}
+            }
+            return SaveChanges();
+        }
+
+        public int Delete(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+            DbSet.Remove(item);
+
+            return SaveChanges();
+        }
+
+        public int Delete(Expression<Func<T, bool>> predicate)
+        {
+            var records = Where(predicate);
+            if (!records.Any())
+            {
+                throw new Exception();
+            }
+            foreach (var record in records)
+            {
+                DbSet.Remove(record);
+            }
+            return SaveChanges();
+        }
+
+        /// <summary>
+        /// Count all item in a DB table.
+        /// </summary>
+        public int Count
+        {
+            get { return DbSet.Count(); }
+        }
+        public long LongCount
+        {
+            get { return DbSet.LongCount(); }
+        }
+        public int CountFunc(Expression<Func<T, bool>> predicate)
+        {
+            return DbSet.Count(predicate);
+        }
+
+        public long LongCountFunc(Expression<Func<T, bool>> predicate)
+        {
+            return DbSet.LongCount(predicate);
+        }
+
+        public bool IsExist(Expression<Func<T, bool>> predicate)
+        {
+            var count = DbSet.Count(predicate);
+            return count > 0;
+        }
+
+        public T First(Expression<Func<T, bool>> predicate)
+        {
+            return DbSet.First(predicate);
+        }
+
+        public T FirstOrDefault(Expression<Func<T, bool>> predicate)
+        {
+            return DbSet.FirstOrDefault(predicate);
+        }
+
+        public T Find(Expression<Func<T, bool>> predicate)
+        {
+            return DbSet.AsNoTracking().FirstOrDefault(predicate);
+        }
+
+        public string Max(Expression<Func<T, string>> predicate)
+        {
+            return DbSet.Max(predicate);
+        }
+
+        public string MaxFunc(Expression<Func<T, string>> predicate, Expression<Func<T, bool>> where)
+        {
+            return DbSet.Where(where).Max(predicate);
+        }
+
+        public string Min(Expression<Func<T, string>> predicate)
+        {
+            return DbSet.Min(predicate);
+        }
+
+        public string MinFunc(Expression<Func<T, string>> predicate, Expression<Func<T, bool>> where)
+        {
+            return DbSet.Where(where).Min(predicate);
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> predicate)
+        {
+            return DbSet.Where(predicate);
+        }
+
+        public IQueryable<T> FindAll(Expression<Func<T, bool>> predicate)
+        {
+            return DbSet.Where(predicate).AsNoTracking().AsQueryable();
+        }
+        #endregion
+
+        #region DATABSE TRANSACTION
+
+        public void Attach<TEntity>(TEntity item) where TEntity : class
+        {
+            _context.Entry(item).State = EntityState.Unchanged;
+        }
+
+        public void SetModified<TEntity>(TEntity item) where TEntity : class
+        {
+            //this operation also attach item in object state manager
+            _context.Entry(item).State = EntityState.Modified;
+        }
+
+        public void ApplyCurrentValues<TEntity>(TEntity original, TEntity current)
+            where TEntity : class
+        {
+            //if it is not attached, attach original and set current values
+            _context.Entry(original).CurrentValues.SetValues(current);
+        }
+
+        public int SaveChanges()
+        {
+            try
+            {
+                return _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException == null) throw new Exception(ex.Message);
+                var message = "1." + ex.Message +
+                                  (ex.InnerException != null
+                                  ? "::2." + ex.InnerException.Message
+                                  : "") +
+                                (ex.InnerException != null && ex.InnerException.InnerException != null
+                                  ? "::3." + ex.InnerException.InnerException.Message
+                                  : "")
+                              ;
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Commit()
+        {
+            SaveChanges();
+        }
+
+        public void CommitAndRefreshChanges()
+        {
+            bool saveFailed;
+
+            do
+            {
+                try
+                {
+                    _context.SaveChanges();
+                    saveFailed = false;
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    saveFailed = true;
+
+                    ex.Entries.ToList()
+                        .ForEach(entry =>
+                        {
+                            entry.OriginalValues.SetValues(entry.GetDatabaseValues());
+                        });
+                }
+            } while (saveFailed);
+        }
+
+        public void RollbackChanges()
+        {
+            _context.ChangeTracker.Entries()
+                .ToList()
+                .ForEach(entry => entry.State = EntityState.Unchanged);
+        }
+        #endregion
+
+        #region IDisposable Members
+        ~Repository()
+        {
+            Dispose(false);
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    if (_context != null)
+                    {
+                        _context.Dispose();
+                        _context = null;
+                    }
+                }
+            }
+            _disposed = true;
+        }
+        #endregion
+
+        #region LINQ ASYNC
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ICollection<T>> GetAllAsync()
+        {
+            return await DbSet.ToListAsync();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public async Task<T> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await DbSet.AsNoTracking().SingleOrDefaultAsync(predicate);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public async Task<ICollection<T>> FindAllAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await DbSet.Where(predicate).AsNoTracking().ToListAsync();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public async Task<T> CreateAsync(T entity)
+        {
+            DbSet.Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public async Task<int> UpdateAsync(T item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+            var entry = _context.Entry(item);
+            DbSet.Attach(item);
+            entry.State = EntityState.Modified;
+            return await _context.SaveChangesAsync();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public async Task<int> UpdateAsync(Expression<Func<T, bool>> predicate)
+        {
+            var records = await DbSet.Where(predicate).ToListAsync();
+            if (!records.Any())
+            {
+                throw new Exception();
+            }
+            foreach (var record in records)
+            {
+                var entry = _context.Entry(record);
+
+                DbSet.Attach(record);
+
+                entry.State = EntityState.Modified;
+            }
+            return await _context.SaveChangesAsync();
+        }
+        public async Task<int> DeleteAsync(T t)
+        {
+            DbSet.Remove(t);
+            return await _context.SaveChangesAsync();
+        }
+        public async Task<int> DeleteAsync(Expression<Func<T, bool>> predicate)
+        {
+            var records = await DbSet.Where(predicate).ToListAsync();
+            if (!records.Any())
+            {
+                throw new Exception();
+            }
+            foreach (var record in records)
+            {
+                DbSet.Remove(record);
+            }
+            return await _context.SaveChangesAsync();
+        }
+        public async Task<int> CountAsync()
+        {
+            return await DbSet.CountAsync();
+        }
+        public async Task<long> LongCountAsync()
+        {
+            return await DbSet.LongCountAsync();
+        }
+        public async Task<int> CountFuncAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await DbSet.CountAsync(predicate);
+        }
+        public async Task<long> LongCountFuncAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await DbSet.LongCountAsync(predicate);
+        }
+        public async Task<T> FirstAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await DbSet.FirstAsync(predicate);
+        }
+        public async Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await DbSet.FirstOrDefaultAsync(predicate);
+        }
+        public async Task<string> MaxAsync(Expression<Func<T, string>> predicate)
+        {
+            return await DbSet.MaxAsync(predicate);
+        }
+        public async Task<string> MaxFuncAsync(Expression<Func<T, string>> predicate, Expression<Func<T, bool>> where)
+        {
+            return await DbSet.Where(where).MaxAsync(predicate);
+        }
+        public async Task<string> MinAsync(Expression<Func<T, string>> predicate)
+        {
+            return await DbSet.MinAsync(predicate);
+        }
+        public async Task<string> MinFuncAsync(Expression<Func<T, string>> predicate, Expression<Func<T, bool>> where)
+        {
+            return await DbSet.Where(where).MinAsync(predicate);
+        }
+        public async Task<bool> IsExistAsync(Expression<Func<T, bool>> predicate)
+        {
+            var count = await DbSet.CountAsync(predicate);
+            return count > 0;
+        }
+        public async Task<int> SaveChangesAsync()
+        {
+            try
+            {
+                return await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        #endregion
+
+        public int UpdateTest<TViewModel>(Expression<Func<T, bool>> predicate, TViewModel item)
+        {
+            if (item == null)
+                throw new ArgumentNullException(nameof(item));
+
+            var saved = DbSet.FirstOrDefault(predicate);
+
+            _context.Entry(saved).CurrentValues.SetValues(item);
+
+            //DbSet.Attach(item);
+            //entry.State = EntityState.Modified;
+            return SaveChanges();
+        }
+    }
+}
