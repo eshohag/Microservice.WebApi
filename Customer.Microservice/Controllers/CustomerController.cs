81d1252f75a40ac5ae72a2579fbac8b72e1491fa
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2021-12-09 13:24:45 +0600

    Architectures

--- Customer.Microservice/Controllers/CustomerController.cs
+++ Customer.Microservice/Controllers/CustomerController.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Customer.Microservice.Application;
 using Customer.Microservice.Domain.Models;
-using Customer.Microservice.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Customer.Microservice.Application.Manager.Interfaces;
+using Customer.Microservice.Infrastructure.Repository.Interfaces;
 
 namespace Customer.Microservice.Controllers
 {
@@ -16,19 +16,20 @@ namespace Customer.Microservice.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
-        private IApplicationDbContext _context;
+        public ICustomerRepository _customerRepository { get; }
+
         private readonly ITestManager _testManager;
 
-        public CustomerController(IApplicationDbContext context, ITestManager testManager)
+        public CustomerController(ICustomerRepository customerRepository, ITestManager testManager)
         {
-            _context = context;
+            _customerRepository = customerRepository;
             _testManager = testManager;
         }
+
         [HttpPost]
         public async Task<IActionResult> Create(Domain.Models.Customer customer)
         {
-            _context.Customers.Add(customer);
-            await _context.SaveChanges();
+            _customerRepository.Add(customer);
             return Ok(customer.Id);
         }
         [HttpGet]
@@ -36,30 +37,30 @@ namespace Customer.Microservice.Controllers
         {
             var test = _testManager.TestMessage();
 
-            var customers = await _context.Customers.ToListAsync();
+            var customers = await _customerRepository.GetAllAsync();
             if (customers == null) return NotFound();
             return Ok(customers);
         }
+
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
-            var customer = await _context.Customers.Where(a => a.Id == id).FirstOrDefaultAsync();
+            var customer = await _customerRepository.Where(a => a.Id == id).FirstOrDefaultAsync();
             if (customer == null) return NotFound();
             return Ok(customer);
         }
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id)
         {
-            var customer = await _context.Customers.Where(a => a.Id == id).FirstOrDefaultAsync();
+            var customer = await _customerRepository.Where(a => a.Id == id).FirstOrDefaultAsync();
             if (customer == null) return NotFound();
-            _context.Customers.Remove(customer);
-            await _context.SaveChanges();
+            _customerRepository.Remove(customer);
             return Ok(customer.Id);
         }
         [HttpPut("{id}")]
         public async Task<IActionResult> Update(int id, Domain.Models.Customer customerData)
         {
-            var customer = _context.Customers.Where(a => a.Id == id).FirstOrDefault();
+            var customer = _customerRepository.Where(a => a.Id == id).FirstOrDefault();
 
             if (customer == null) return NotFound();
             else
@@ -68,7 +69,6 @@ namespace Customer.Microservice.Controllers
                 customer.Name = customerData.Name;
                 customer.Contact = customerData.Contact;
                 customer.Email = customerData.Email;
-                await _context.SaveChanges();
                 return Ok(customer.Id);
             }
         }
