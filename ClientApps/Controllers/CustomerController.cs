52f79b14c30610b9b24f84a866ed56106e498480
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2022-02-07 10:21:06 +0600

    PageList

--- ClientApps/Controllers/CustomerController.cs
+++ ClientApps/Controllers/CustomerController.cs
@@ -2,7 +2,6 @@
 using ClientApps.Filters;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace ClientApps.Controllers
@@ -12,10 +11,10 @@ namespace ClientApps.Controllers
         [Authorize]
         public async Task<IActionResult> Index([FromQuery] FilterOption filter)
         {
-            var customers = await HttpClientHelper.GetAsync<List<Customer>>(url: "https://localhost:44382/gateway/customer/getall", token: Settings.JwtToken);
+            //var customers = await HttpClientHelper.GetAsync<List<Customer>>(url: "https://localhost:44382/gateway/customer/getall", token: Settings.JwtToken);
             var models = await HttpClientHelper.GetAsync<PaginatedList<Customer>>(url: "https://localhost:44382/gateway/customer/all?pageNumber=" + filter.PageNumber + "&pageSize=" + filter.PageSize, token: Settings.JwtToken);
 
-            return View(customers);
+            return View(models);
         }
     }
 }
--- ClientApps/Startup.cs
+++ ClientApps/Startup.cs
@@ -1,14 +1,10 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ClientApps
 {
@@ -23,7 +19,9 @@ namespace ClientApps
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddMvcCore().AddJsonOptions(op => op.JsonSerializerOptions.PropertyNamingPolicy = null);
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                     .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
                     {
