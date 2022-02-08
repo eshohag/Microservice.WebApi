bb00621e6617f121019cb575aa29bdbca1b1c903
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2022-02-08 14:13:17 +0600

    Documents Updates

--- ClientApps/Controllers/CustomerController.cs
+++ ClientApps/Controllers/CustomerController.cs
@@ -11,8 +11,8 @@ namespace ClientApps.Controllers
         [Authorize]
         public async Task<IActionResult> Index([FromQuery] FilterOption filter)
         {
-            //var customers = await HttpClientHelper.GetAsync<List<Customer>>(url: "https://localhost:44382/gateway/customer/getall", token: Settings.JwtToken);
             var models = await HttpClientHelper.GetAsync<PaginatedList<Customer>>(url: "https://localhost:44382/gateway/customer/all?pageNumber=" + filter.PageNumber + "&pageSize=" + filter.PageSize, token: Settings.JwtToken);
+            var userData = await HttpClientHelper.GetAsync<PaginatedList<Customer>>(url: "https://localhost:44382/gateway/customer/all?pageNumber=" + filter.PageNumber + "&pageSize=" + filter.PageSize, token: Settings.JwtToken);
 
             return View(models);
         }
